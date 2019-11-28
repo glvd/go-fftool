821b5bb8cbec911ef928b85533a0578c5c330602
godcong <jumbycc@163.com>
2019-11-28 18:05:07 +0800

    update go-fftool

--- command.go
+++ command.go
@@ -67,7 +67,7 @@ func (c *Command) AddArgs(s string) {
 func getCurrentDir() string {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0])) //返回绝对路径  filepath.Dir(os.Args[0])去除最后一个元素的路径
 	if err != nil {
-		log.Fatal(err)
+		log.Errorw("current dir", "error", err)
 		return ""
 	}
 	return dir
