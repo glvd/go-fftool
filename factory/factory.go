fd814eb5add385d3c42838afcee71c24323094ac
godcong <jumbycc@163.com>
2020-01-13 16:16:22 +0800

    update go-fftool

--- factory/factory.go
+++ factory/factory.go
@@ -26,9 +26,32 @@ func new() *Factory {
 }
 
 // Initialize ...
-func Initialize(ops ...Options) {
+func Initialize(opts ...Options) {
+	dop := DefaultOption()
+	for _, op := range opts {
+		op(dop)
+	}
+
+	if dop.MpegName != "" {
+		fftool.DefaultMpegName = dop.MpegName
+	}
+
+	if dop.ProbeName != "" {
+		fftool.DefaultProbeName = dop.ProbeName
+	}
+
 	_factory.once.Do(func() {
 		_factory.probe = fftool.NewFFProbe()
 		_factory.mpeg = fftool.NewFFMpeg()
 	})
+}
+
+// Mpeg ...
+func Mpeg() *fftool.FFMpeg {
+	return _factory.mpeg
+}
+
+// Probe ...
+func Probe() *fftool.FFProbe {
+	return _factory.probe
 }
