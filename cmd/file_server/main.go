19a30f626611e9c83b80053f4d75eda64cc803cc
godcong <jumbycc@163.com>
2021-02-07 16:55:15 +0800

    update go-fftool

--- cmd/file_server/main.go
+++ cmd/file_server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var dir = `D:\workspace\golang\project\go-fftool\video_split_temp`
@@ -11,10 +12,9 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.Default())
-	router.Static("/", dir)
-
+	router.Handle(http.MethodGet, "/*path", gin.WrapH(http.FileServer(http.Dir(dir))))
+	//router.Static("/*", "dir")
 	if err := router.Run("0.0.0.0:8081"); err != nil {
 		panic(err)
 	}
-
 }
